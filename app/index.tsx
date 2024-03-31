9fbbecf389d7c2b41d5c21f67dc74c0ab3b4a3eb
Luke Grayland <lgrayland96@gmail.com>
2024-03-31 23:26:02 +0100

    added rnui

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,8 @@
 import {Button, Text, View} from 'react-native'
 import ProfileButton from "../components/buttons/profileButton";
-import {GetAllSales} from "../dataAccess/getAllSales";
-import {useState} from "react";
-import {SaleModel} from "../models/saleModel";
-import {GetSalesByLocation} from "../dataAccess/getSalesByLocation";
-import {GetSaleById} from "../dataAccess/getSaleById";
-import {GetSalesByPhrase} from "../dataAccess/getSalesByPhrase";
-import {GetSalesByRegion} from "../dataAccess/getSalesByRegion";
 
 const Home = () => {
-    const [sales, setSales] = useState<SaleModel[]>()
-    
-    const handlePress = async () => {
+    const handleLogIn = async () => {
         
     }
     
@@ -19,7 +10,7 @@ const Home = () => {
         <View>
             <Text>Home screen</Text>
             <ProfileButton/>
-            <Button title="getAllSales" onPress={handlePress}/>
+            <Button title="Log in" onPress={handleLogIn}/>
         </View>
     )
 }
